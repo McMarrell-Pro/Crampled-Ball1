2139768c47564dd45b3982e8be2eeab1cee9b045
McMarrell-Pro <64479501+McMarrell-Pro@users.noreply.github.com>
2021-01-01 20:55:42 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
 var bar1, bar2, bar3;
+var ground;
+var paper;
 
 function preload()
 {
@@ -19,8 +20,13 @@ function setup() {
 	world = engine.world;
 
 	//Create the Bodies Here.
-	bar1 = new Dustbin(550,600,20,60);
-	bar2 = new Dustbin(750,600,20,60);
+	bar1 = new Dustbin(540,600,20,140);
+	bar2 = new Dustbin(760,600,20,140);
+	bar3 = new Dustbin(650,660,200,20);
+
+	ground = new Ground(400,690,800,20);
+
+	paper = new Paper(400,350,10);
 
 
 	Engine.run(engine);
@@ -36,6 +42,11 @@ function draw() {
 
   bar1.display();
   bar2.display();
+  bar3.display();
+
+  ground.display();
+
+  paper.display();
  
 }
 
